b2507f1cb9fd84a2dadfa7df479fe8e4493b36b7
Samantha <693487909@qq.com>
2017-08-07 20:21:24 +0800

    update .gitnore

---
+++ app/src/main/java/chinasoft/com/logindemo/ChangeActivity.java
@@ -0,0 +1,15 @@
+package chinasoft.com.logindemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+//@ContentView(R.layout.activity_change)
+public class ChangeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change);
+       // private Button adminlogin;
+    }
+}
